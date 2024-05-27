c6d3cac94d130e979b7a39b9308bea99e975578f
VGoychev <144837107+VGoychev@users.noreply.github.com>
2024-05-27 22:29:15 +0300

    Initial commit

---
+++ src/ArraysExercise/P06_EqualSums.java
@@ -0,0 +1,32 @@
+package ArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P06_EqualSums {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        boolean isArrayFind = false;
+        for (int i = 0; i < numbers.length; i++) {
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int j = 0; j < i; j++) {
+                leftSum += numbers[j];
+            }
+            for (int j = i + 1; j < numbers.length; j++) {
+                rightSum += numbers[j];
+            }
+            if (leftSum == rightSum) {
+                System.out.printf("%d", i);
+                isArrayFind = true;
+            }
+        }
+        if (!isArrayFind) {
+            System.out.println("no");
+        }
+    }
+}
