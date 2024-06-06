57901e29b2a98b5b7b4d3f33b3b02f9732e8746b
VGoychev <144837107+VGoychev@users.noreply.github.com>
2024-06-06 20:32:05 +0300

    Initial commit

--- src/MethodsExercise/P09_PalindromeIntegers.java
+++ src/MethodsExercise/P09_PalindromeIntegers.java
@@ -3,8 +3,23 @@ package MethodsExercise;
 import java.util.Scanner;
 
 public class P09_PalindromeIntegers {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String input = scanner.nextLine();
+        palindromeInt(input);
+    }
+    static void palindromeInt(String number){
+        while (!number.equals("END")) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(number);
+            stringBuilder.reverse();
+            if (number.contentEquals(stringBuilder)){
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            number = scanner.nextLine();
+        }
     }
 }
