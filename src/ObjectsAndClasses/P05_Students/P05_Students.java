ccb6ea00c988b80bf5a3c885f64e9356f1539aba
VGoychev <144837107+VGoychev@users.noreply.github.com>
2024-06-28 15:36:40 +0300

    Initial commit

--- src/ObjectsAndClasses/P05_Students/P05_Students.java
+++ src/ObjectsAndClasses/P05_Students/P05_Students.java
@@ -1,4 +1,38 @@
 package ObjectsAndClasses.P05_Students;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class P05_Students {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Student> students = new ArrayList<>();
+        while (true){
+            String command = scanner.nextLine();
+            if (command.equals("end")){
+                break;
+            }
+            String[] tokens = command.split(" ");
+            String firstName = tokens[0];
+            String lastName = tokens[1];
+            int age = Integer.parseInt(tokens[2]);
+            String homeTown = tokens[3];
+
+            Student currentStudent = new Student();
+
+            currentStudent.setFirstName(firstName);
+            currentStudent.setLastName(lastName);
+            currentStudent.setAge(age);
+            currentStudent.setHomeTown(homeTown);
+
+            students.add(currentStudent);
+        }
+        String town = scanner.nextLine();
+        for (Student student : students){
+            if (student.getHomeTown().equals(town)){
+                System.out.printf("%s %s is %d years old%n",student.getFirstName(), student.getLastName(), student.getAge());
+            }
+        }
+    }
 }
