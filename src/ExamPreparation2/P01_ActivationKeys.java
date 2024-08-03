b97a9d04cfa42d5b8b066524669cc4799270f3ac
VGoychev <144837107+VGoychev@users.noreply.github.com>
2024-08-03 23:38:09 +0300

    Initial commit

--- src/ExamPreparation2/P01_ActivationKeys.java
+++ src/ExamPreparation2/P01_ActivationKeys.java
@@ -1,4 +1,59 @@
 package ExamPreparation2;
 
+import java.util.Scanner;
+
 public class P01_ActivationKeys {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder activationKey = new StringBuilder(scanner.nextLine());
+        String input = scanner.nextLine();
+
+        while (!input.equals("Generate")) {
+            String[] token = input.split(">>>");
+            String command = token[0];
+
+            switch (command) {
+                case "Contains":
+                    String word = token[1];
+                    if (activationKey.toString().contains(word)) {
+                        System.out.printf("%s contains %s%n", activationKey, word);
+                    } else {
+                        System.out.println("Substring not found!");
+                    }
+                    break;
+
+                case "Flip":
+                    String move = token[1];
+                    int start = Integer.parseInt(token[2]);
+                    int end = Integer.parseInt(token[3]);
+                    String substring = activationKey.substring(start, end);
+                    String transformedSubstring = "";
+
+                    if (move.equals("Upper")) {
+                        transformedSubstring = substring.toUpperCase();
+                    } else if (move.equals("Lower")) {
+                        transformedSubstring = substring.toLowerCase();
+                    }
+
+                    activationKey.replace(start, end, transformedSubstring);
+                    System.out.println(activationKey);
+                    break;
+
+                case "Slice":
+                    int startIndex = Integer.parseInt(token[1]);
+                    int endIndex = Integer.parseInt(token[2]);
+
+                    if (startIndex >= 0 && endIndex <= activationKey.length()) {
+                        activationKey.delete(startIndex, endIndex);
+                    }
+
+                    System.out.println(activationKey);
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Your activation key is: " + activationKey);
+    }
 }
--- src/ExamPreparation2/P02_FancyBarcodes.java
+++ src/ExamPreparation2/P02_FancyBarcodes.java
@@ -1,4 +1,41 @@
 package ExamPreparation2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class P02_FancyBarcodes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        String regex = "@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+";
+        Pattern pattern = Pattern.compile(regex);
+        List<String> productGroup = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
+            if (!matcher.find()){
+                System.out.println("Invalid barcode");
+            } else {
+                String current = "";
+                for (int index = 0 ; index <= input.length() - 1; index++) {
+                    char symbol = input.charAt(index);
+                    if (Character.isDigit(symbol)){
+                        current += symbol;
+                    }
+                }
+                if (current.isEmpty()){
+                    System.out.println("Product group: 00");
+                    String emptyGroup = "00";
+                    productGroup.add(emptyGroup);
+                } else {
+                    System.out.println("Product group: " + current);
+                    productGroup.add(current);
+                }
+            }
+
+        }
+    }
 }
