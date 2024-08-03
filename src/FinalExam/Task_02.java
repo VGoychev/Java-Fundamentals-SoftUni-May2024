2390d3c878663d2c3d655d08ec8ef4002f421863
VGoychev <144837107+VGoychev@users.noreply.github.com>
2024-08-03 23:40:12 +0300

    Initial commit

---
+++ src/FinalExam/Task_02.java
@@ -0,0 +1,10 @@
+package FinalExam;
+
+import java.util.Scanner;
+
+public class Task_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
